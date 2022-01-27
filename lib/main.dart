9aa45f217fcb1771dcd913585a8f60b9b297b810
AbdelrahmanMagdy84 <boodyboo84@gmail.com>
2022-01-27 05:20:29 +0200

    addig device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:expenses_chart_app/widgets/newTransaction.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import './screens/homeScreen.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +10,12 @@ import 'models/transaction_model.dart';
 void main() {
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  runApp(ExpensesApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => ExpensesApp(), // Wrap your app
+    ),
+  );
 }
 
 class ExpensesApp extends StatelessWidget {
@@ -15,6 +23,9 @@ class ExpensesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         primaryColor: Colors.brown[100],
         primarySwatch: Colors.brown,
@@ -50,34 +61,80 @@ class _BuildScafoldState extends State<BuildScafold> {
     });
   }
 
-  var appBar = AppBar(
-    title: const Text('Expenses App'),
-    backgroundColor: Colors.brown[100],
-  );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar,
-      body: transactions.isEmpty
-          ? const Center(
-              child: Text('Press + start adding your expenses'),
+    final PreferredSizeWidget appBar;
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      appBar = CupertinoNavigationBar(
+        middle: Text('Expenses App'),
+        trailing: GestureDetector(
+          child: Icon(
+            CupertinoIcons.add,
+          ),
+          onTap: () {
+            startAddNewTransaction(context);
+          },
+        ),
+      );
+    } else {
+      appBar = AppBar(
+        title: const Text('Expenses App'),
+        backgroundColor: Colors.brown[100],
+        actions: [
+          IconButton(
+            onPressed: () {
+              startAddNewTransaction(context);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      );
+    }
+    Widget pageBody = SafeArea(
+      child: transactions.isEmpty
+          ? Center(
+              child: Text(
+                'Press + start adding your expenses',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
             )
           : HomeScreen(
-              transactions, deleteTransaction, appBar.preferredSize.height),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          startAddNewTransaction(
-            context,
-          );
-        },
-        backgroundColor: Colors.brown[100],
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+              transactions,
+              deleteTransaction,
+              appBar.preferredSize.height,
+            ),
     );
+
+    return Theme.of(context).platform == TargetPlatform.iOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: appBar as ObstructingPreferredSizeWidget,
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: pageBody,
+            floatingActionButton:
+                TargetPlatform.iOS == Theme.of(context).platform
+                    ? Container()
+                    : FloatingActionButton(
+                        onPressed: () {
+                          startAddNewTransaction(
+                            context,
+                          );
+                        },
+                        backgroundColor: Colors.brown[100],
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 
   void startAddNewTransaction(
